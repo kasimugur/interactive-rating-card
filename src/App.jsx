7938667d34a809d90eb7f43edca5ad8aedcdefd0
kasimugur <kasiugur7@gmail.com>
2024-07-07 23:16:20 +0300

    css and scss add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Card from "./components/Card"
+import './app.css'
 function App() {
 
   return (
     <>
-    
+    <div className="App">
+      <Card />
+    </div>
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import './css/card.css'
+export default function Card() {
+  return (
+    <>
+      <div className="container">
+        <div className="box">
+          <div className="icon">
+            <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614" /></svg>
+          </div>
+          <h1>How did we do?</h1>
+          <p>Please let us know how we did with your support request. Allfeedback is appreciated to help us improve our offering!</p>
+          <div className="span">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+          </div>
+          <button>SUBMIT</button>
+        </div>
+      </div>
+    </>
+  )
+}
