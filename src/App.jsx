3b6fd5076d74aef9e31a3207f4cfa613b4b89de9
kasimugur <kasiugur7@gmail.com>
2024-07-08 17:49:57 +0300

    project codes are finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Card from "./components/Card"
 import './app.css'
 import CardEnd from "./components/CardEnd"
+import { useState } from "react"
+import { SiteContext } from "./Context"
 function App() {
+  const [select, setSelect] = useState(0)
+
+
+  const data = { setSelect, select }
+
 
   return (
     <>
-    <div className="App">
-      <Card />
-{/* <CardEnd />
- */}
-    </div>
+      <SiteContext.Provider value={data}>
+        <div className="App">
+          {select === 0 && <Card /> || select !== 0 && <CardEnd />}
+        </div>
+      </SiteContext.Provider>
     </>
   )
 }
