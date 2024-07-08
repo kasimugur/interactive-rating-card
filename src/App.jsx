50769dd5df493926cddd60ff80fc791cbc2e5c55
kasimugur <kasiugur7@gmail.com>
2024-07-08 15:33:06 +0300

    first css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Card from "./components/Card"
 import './app.css'
+import CardEnd from "./components/CardEnd"
 function App() {
 
   return (
     <>
     <div className="App">
       <Card />
+{/* <CardEnd />
+ */}
     </div>
     </>
   )
