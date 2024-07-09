214809d81540e358ad37f9774bcbf99d9b866100
kasimugur <kasiugur7@gmail.com>
2024-07-09 17:46:56 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
 
 
   const data = { setSelect, select }
-  console.log(select)
 
   return (
     <>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react'
 import './css/card.css'
 import { SiteContext } from '../Context'
 export default function Card() {
-  const { setSelect } = useContext(SiteContext)
+  const { select, setSelect } = useContext(SiteContext)
   const [selectedText, setSelectedText] = useState('')
   const [activeButton, setActiveButton] = useState(null)
 
@@ -16,7 +16,6 @@ export default function Card() {
   }
   const handleClick = () => {
     setSelect(selectedText)
-
   }
 
   const spanColor = {
